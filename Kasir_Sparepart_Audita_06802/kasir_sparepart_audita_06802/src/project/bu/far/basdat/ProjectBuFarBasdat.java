17c6f0499a77e5d789ef8f6ec0f3e21a9454a5b6
Audita Taufiqurrahman <34263454+omaannn@users.noreply.github.com>
2019-09-29 23:41:32 +0700

    Add files via upload

---
+++ Kasir_Sparepart_Audita_06802/kasir_sparepart_audita_06802/src/project/bu/far/basdat/ProjectBuFarBasdat.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.bu.far.basdat;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author BAGAS
+ */
+public class ProjectBuFarBasdat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+        // TODO code application logic here
+        new login().setVisible(true);
+    }
+
+}
